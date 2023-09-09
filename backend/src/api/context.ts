b2ecefb872603bf26b98e12d4d75bd6c5e965de3
Adam Glauser <aglauser@wlu.ca>
2023-09-09 12:24:44 -0400

    Require auth only for mutations

--- backend/src/api/context.ts
+++ backend/src/api/context.ts
@@ -8,41 +8,29 @@ import { IncomingMessage, OutgoingMessage } from "http";
 import { GraphQLError } from "graphql";
 import dotenv from 'dotenv'
 import { FirebaseAuthProvider } from "./auth/FirebaseAuthProvider";
+import { BaseContext } from "@apollo/server";
 
 dotenv.config()
 const adaptor = process.env.USE_LEGACY_ENDPOINT
     ? new LegacyAdaptor(process.env.USE_LEGACY_ENDPOINT || "")
     : new PrismaDataAdaptor()
 
-export interface Context {
+export interface Context extends BaseContext {
     db: IDataAdaptor,
-    uid: number | null
+    uid: Promise<number | null>
 }
 
 const auth = new FirebaseAuthProvider(process.env, adaptor.userDAO);
 
-const getAuthorizedUserID = async (req: IncomingMessage): Promise<number> => {
-    const user = await auth.getUserId(req.headers.authorization)
-    if (user === undefined || user == null) {
-        //TODO: figure out how to make this actually return to client instead of
-        //  logging error to console and client request times out
-        throw new GraphQLError('You must be logged in to access the API', {
-            extensions: {
-                code: 'UNAUTHENTICATED',
-                http: { status: 401 }
-            }
-        })
-    }
-    return user
-}
+const getAuthorizedUserID = (req: IncomingMessage): Promise<number | null> => auth.getUserId(req.headers.authorization)
 
-export const createContext = async ({ req, res }: { req: IncomingMessage, res: OutgoingMessage }) => {
+export const createContext = ({ req, res }: { req: IncomingMessage, res: OutgoingMessage }): Context => {
     const timerName = `create_context${Date.now()}`
     console.time(timerName)
     try {
         const context = {
             db: adaptor,
-            uid: await getAuthorizedUserID(req)
+            uid: getAuthorizedUserID(req)
         }
         return context
     }
@@ -50,4 +38,3 @@ export const createContext = async ({ req, res }: { req: IncomingMessage, res: O
         console.timeEnd(timerName)
     }
 }
-
