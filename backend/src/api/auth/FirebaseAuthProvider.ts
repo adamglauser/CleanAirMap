d686e822f75894531b632f2cf421d915f995bdd9
Adam Glauser <aglauser@wlu.ca>
2023-09-15 09:27:42 -0400

    Reduce the awaits in the FirebaseAuthProvider

--- backend/src/api/auth/FirebaseAuthProvider.ts
+++ backend/src/api/auth/FirebaseAuthProvider.ts
@@ -4,6 +4,7 @@ import { IAuthProvider, UserModel } from "./IAuthProvider"
 import { IUser } from "../../dataAccess/IUser"
 import { UserRecord } from "firebase-admin/lib/auth/user-record"
 import { GraphQLError } from "graphql"
+import { Auth } from "firebase-admin/lib/auth/auth"
 
 async function initFirebase(env : NodeJS.ProcessEnv) : Promise<firebaseAdmin.app.App> {
     const creds = await import(process.env.FIREBASE_CREDS_PATH ?? './firebase_creds.json')
@@ -12,49 +13,47 @@ async function initFirebase(env : NodeJS.ProcessEnv) : Promise<firebaseAdmin.app
 }
 
 export class FirebaseAuthProvider implements IAuthProvider {
-    adminApp : Promise<firebaseAdmin.app.App>
     userDAO : IUser
+    auth : Auth | undefined
+    adminApp : Promise<firebaseAdmin.app.App>
 
     constructor(env : NodeJS.ProcessEnv, userDAO : IUser) {
         this.adminApp = initFirebase(env)
+        this.adminApp.then((app) => this.auth = app.auth())
+        this.auth = undefined
         this.userDAO = userDAO
     }
+
+    async getAuth() {
+        if (!this.auth) {
+            this.auth = (await this.adminApp).auth()
+        }
+        return this.auth
+    }
+
     async getUser(authHeader : String | undefined) : Promise<UserModel | null> {
         if (!authHeader) { return null }
 
         const jwt = authHeader.split(" ")[1]
-        if (!jwt) {
-            throw new GraphQLError("Invalid Authorization header: should be 'Bearer: <your token>'")
-        }
 
-        let decodedToken : DecodedIdToken | null = null
-        let firebaseUser : UserRecord | null = null
-        try {
-            const authService = (await this.adminApp).auth()
-            decodedToken = await authService.verifyIdToken(jwt)
-            firebaseUser = await authService.getUser(decodedToken.uid)
-        }
-        catch (e) {
-            console.error(e)
-        }
-        finally {
-            if (decodedToken == null || firebaseUser == null) { return null }
-        }
-
-        return this.userDAO.getByExternalId(decodedToken.uid).then((user) => {
-            if (user == null) {
-                //@ts-ignore - ID is not required on create
-                return this.userDAO.create( {
-                    name: firebaseUser?.displayName ?? "Unknown",
-                    email: firebaseUser?.email ?? "unknown@example.com",
-                    externalAuthId: decodedToken?.uid ?? null,
-                    externalAuthType: "FirebaseAuthProvider"
-                })
-            }
-            else {
-                return user
-            }
-        })
+        return this.getAuth()
+            .then((auth) => auth.verifyIdToken(jwt))
+            .then(async (decodedIdToken) => {
+                const user = this.userDAO.getByExternalId(decodedIdToken.uid)
+                if (user == null) {
+                    const firebaseUser = await this.auth?.getUser(decodedIdToken.uid)
+                    //@ts-ignore - ID is not required on create
+                    return this.userDAO.create( {
+                        name: firebaseUser?.displayName ?? "Unknown",
+                        email: firebaseUser?.email ?? "unknown@example.com",
+                        externalAuthId: decodedIdToken?.uid ?? null,
+                        externalAuthType: "FirebaseAuthProvider"
+                    })
+                }
+                else {
+                    return user
+                }
+            })
     }
     async getUserId(authHeader : String | undefined) : Promise<number | null> {
         const user = await this.getUser(authHeader)
