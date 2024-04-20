dd6533762dad21ceced1a017964df3b8fbb2e04a
Adam Glauser <aglauser@wlu.ca>
2024-04-20 11:00:33 -0400

    Get v1 location data and write to file

--- tools/migration/index.mjs
+++ tools/migration/index.mjs
@@ -1,13 +1,23 @@
 import { request, gql, GraphQLClient } from 'graphql-request';
+import * as fs from 'node:fs';
+import { setTimeout } from "timers/promises"
 const graphQLClient = new GraphQLClient('https://gateway.raventalk.org/graphql');
 
+var locationsFilePath = './locations.json';
+console.log("Preparing query of locations")
 const locationQuery = gql`
       query GetLocations {
         locations {
           id
           name
+          address
+          latitude
+          longitude
         }
       }
     `;
-const results = await graphQLClient.request(locationQuery);
-console.log("Location query results: " + JSON.stringify(results));
+const v1Locations = await graphQLClient.request(locationQuery);
+console.log("Retrieved %d locations", v1Locations.locations.length);
+
+console.log("Writing to file %s", locationsFilePath);
+fs.writeFileSync(locationsFilePath, JSON.stringify(v1Locations));
